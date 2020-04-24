be35f8649fb924239f839db4f7f97eef0f36dc41
Henrique Poleselo <hpoleselo@gmail.com>
2020-04-23 23:21:52 -0300

    Colocando o rosto mas nao em todas as pessoas, arrumar a estrutura do for para fazer em todos

--- src/face_detection.py
+++ src/face_detection.py
@@ -3,44 +3,55 @@ import cv2
 
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-
 # Se der empty eh pq o face cascade nao esta sendo carregado
 #print(face_cascade.empty())
 
-babu = cv2.imread('babu.jpg')
+babu = cv2.imread('babu.png', -1)
 img = cv2.imread('photo1.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+babu_height, babu_width = babu.shape[0:2] 
 
 # Ja retorna uma matriz Nx4 onde N eh o numero de faces detectadas
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+#TODO: VER PARAMETRO DE ESTICAMENTO DO BABU (1.3 em y pra ajustar o encaixe da face) entra
+#TODO: tamanho variar de acordo com o tamanho do rosto
+
 # No caso de 3 faces, o for fara 3 iteracoes
-for (x,y,w,h) in faces: 
+for (x,y,w,h) in faces:
+    #babuzinho = cv2.resize(babu, (w, h))
+    babuzinho = cv2.resize(babu, (255, 255))
+    bg_img = img.copy()
+    b, g, r, alpha = cv2.split(babuzinho)
+    babu_colorido = cv2.merge((b,g,r))
 
-    # tamanho do rosto
-    print(x)
-    print(w)
-    
-    # tamanho do rosto
-    size_x = int(x) - int(w)
-    print(type(size_x))
-    
-    #size_x = xtmp - wtmp
-    print("Tamanho do rosto em x: "), size_x
-    size_y = y - h
+    mascara = cv2.medianBlur(alpha,5)
 
-    # de acordo com as medidas do rosto iriamos entao ajustar com um resizing/rescaling
-    # pega a foto do babu
-    #print("Tamanho do rosto em x: "), size_x
-    #print("Tamanho do rosto em y: "), size_y
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    #eyes = eye_cascade.detectMultiScale(roi_gray)
-    #for (ex,ey,ew,eh) in eyes:
-    #    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    # s
+    h,w,_ = babu_colorido.shape
 
-cv2.imshow('img',img)   
+    # area de interesse
+    roi = img[y:y+h,x:x+w]
+    # Black-out the area behind the logo in our original ROI
+    img1_bg = cv2.bitwise_and(roi.copy(),roi.copy(),mask=cv2.bitwise_not(mascara))
+
+    # Mask out the logo from the logo image.
+    img2_fg = cv2.bitwise_and(babu_colorido,babu_colorido,mask = mascara)
+
+
+    # Update the original image with our new ROI
+    bg_img[y:y+h, x:x+w] = cv2.add(img1_bg, img2_fg)
+
+
+# Desenha as bounding boxes reconhecidas
+#img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+#roi_gray = gray[y:y+h, x:x+w]
+#roi_color = img[y:y+h, x:x+w]
+
+#cv2.imshow('img',babu)   
+#cv2.imshow('babuzinh', babuzinho)
+#cv2.imshow('imgweighted', img_final)
+#cv2.imshow('famiglia', img) 
+cv2.imshow('osso', bg_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
